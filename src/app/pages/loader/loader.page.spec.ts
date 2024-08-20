76aaa60be57239808ea7f86c433f61bcc84dab0e
Salsabila <salsabilla.fadilla13@gmail.com>
2024-08-21 01:30:07 +0700

    video 7

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoaderPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    router = TestBed.get(Router);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    component = fixture.componentInstance;
+  }));
+
+  it('should go to login page after load', fakeAsync(() => {
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    tick(1500);
+
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
+  }))
 });
--- src/app/pages/register/register.page.spec.ts
+++ src/app/pages/register/register.page.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { Router } from '@angular/router';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router: Router;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegisterPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    router = TestBed.get(Router);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    component = fixture.componentInstance;
+  }));
+
+  it('should go to home page on register', () => {
+    spyOn(router, 'navigate');
+
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
   });
 });
